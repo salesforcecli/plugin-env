8414773f61c9327d9a90943fd901232d36990339
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2022-03-15 21:23:36 -0500

    Update src/commands/env/delete/sandbox.ts
    Co-authored-by: Rodrigo Espinosa de los Monteros <1084688+RodEsp@users.noreply.github.com>

--- src/commands/env/delete/sandbox.ts
+++ src/commands/env/delete/sandbox.ts
@@ -4,13 +4,6 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
-/*
- * Copyright (c) 2020, salesforce.com, inc.
- * All rights reserved.
- * Licensed under the BSD 3-Clause license.
- * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
- */
 import { Messages } from '@salesforce/core';
 import { SfCommand, Flags } from '@salesforce/sf-plugins-core';
 import { confirm } from '../../../confirm';

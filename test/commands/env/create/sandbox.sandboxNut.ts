be7606f2b8ce90ec609bac141e8002d1f04344fd
Peter Hale <peter.hale@salesforce.com>
2022-03-18 11:38:43 -0600

    Revert "chore: run sandbox nuts [sb-nuts]"
    This reverts commit f4c014f7559c34a6378a94a6563e0b959cdce699.

--- test/commands/env/create/sandbox.sandboxNut.ts
+++ test/commands/env/create/sandbox.sandboxNut.ts
@@ -4,7 +4,6 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
 import { execCmd, TestSession } from '@salesforce/cli-plugins-testkit';
 import { Lifecycle, Messages, SandboxEvents, SandboxProcessObject, SfError, StatusEvent } from '@salesforce/core';
 import { expect } from 'chai';

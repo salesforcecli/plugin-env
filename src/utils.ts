68d6edcda726755f977882eb73b23f361bc30838
mshanemc <shane.mclaughlin@salesforce.com>
2023-12-14 15:20:17 -0600

    fix: remove ts-types wrapper

--- src/utils.ts
+++ src/utils.ts
@@ -5,13 +5,12 @@
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
 
-import { isArray } from '@salesforce/ts-types';
 import { capitalCase } from 'change-case';
 import { JsonObject } from '@salesforce/sf-plugins-core';
 
 export function toValue(val: JsonObject[keyof JsonObject]): string | boolean | number {
   if (!val && val !== false) return '';
-  if (isArray(val)) return val.join(', ');
+  if (Array.isArray(val)) return val.join(', ');
   return val;
 }
 
